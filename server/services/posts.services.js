47c8ec07a01fdd212c9c9699a7ce7176c8749a69
Mohsen Dorparasti <dorparasti@gmail.com>
2023-03-30 22:09:30 +0430

    implemented posts.services.buildGetFilter()

--- server/services/posts.services.js
+++ server/services/posts.services.js
@@ -3,3 +3,36 @@ module.exports.buildGetParameters = function (req, params) {
   params.display = true;
   return params;
 };
+
+module.exports.buildGetFilter = function (parameters) {
+  const filter = {};
+
+  for (const param in parameters) {
+    if (parameters[param] === undefined || parameters[param] === null) continue;
+
+    switch (true) {
+      case ["_id", "display"].includes(param):
+        filter[param] = parameters[param];
+        break;
+
+      case [
+        "author",
+        "subTitle",
+        "title",
+        "content",
+        "urlTitle",
+        "tags",
+      ].includes(param):
+        filter[param] = new RegExp(parameters[param], "i");
+        break;
+
+      case param == "publish":
+        filter[param] = {
+          $gte: parameters[param].from,
+          $lte: parameters[param].to,
+        };
+        break;
+    }
+  }
+  return filter;
+};
--- server/tests/unit/services/posts.services.test.js
+++ server/tests/unit/services/posts.services.test.js
@@ -11,7 +11,64 @@ describe("buildGetParameters", () => {
   it("should add display:true to params if user is not authenticated", () => {
     req.user.isAuthenticated = false;
     const result = postsSrv.buildGetParameters(req, params);
-    console.log(params, result);
     expect(result).toHaveProperty("display");
+  });
+});
+
+describe("buildGetFilter", () => {
+  it("should use the sent value for _id and display", () => {
+    const filter = postsSrv.buildGetFilter({ _id: 1, display: true });
+    expect(filter).toHaveProperty("_id", 1);
+    expect(filter).toHaveProperty("display", true);
+  });
+
+  it("should use regex for string props", () => {
+    const filter = postsSrv.buildGetFilter({
+      author: "author",
+      subTitle: "subTitle",
+      title: "title",
+      content: "content",
+      urlTitle: "urlTitle",
+      tags: "tags",
+    });
+
+    expect(filter).toHaveProperty("author", /author/i);
+    expect(filter).toHaveProperty("subTitle", /subTitle/i);
+    expect(filter).toHaveProperty("title", /title/i);
+    expect(filter).toHaveProperty("content", /content/i);
+    expect(filter).toHaveProperty("urlTitle", /urlTitle/i);
+    expect(filter).toHaveProperty("tags", /tags/i);
+  });
+
+  it("should use $gte and $lte for publish", () => {
+    const filter = postsSrv.buildGetFilter({
+      publish: { from: "12/12/2022", to: "12/12/2022" },
+    });
+    expect(filter).toHaveProperty("publish", {
+      $gte: "12/12/2022",
+      $lte: "12/12/2022",
+    });
+  });
+
+  it("should return empty object if an empty object is provided", () => {
+    const filter = postsSrv.buildGetFilter({});
+    expect(filter).toEqual({});
+  });
+
+  it("should return empty object if no parameter is provided", () => {
+    const filter = postsSrv.buildGetFilter();
+    expect(filter).toEqual({});
+  });
+
+  it("should skip parameter if value is undefined or null", () => {
+    const filter = postsSrv.buildGetFilter({
+      _id: undefined,
+      title: null,
+      display: true,
+    });
+
+    expect(filter).toHaveProperty("display", true);
+    expect(filter).not.toHaveProperty("_id");
+    expect(filter).not.toHaveProperty("title");
   });
 });
