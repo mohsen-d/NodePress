9b9792134e655265df4162e67b4158dbdfe3568a
Mohsen Dorparasti <dorparasti@gmail.com>
2023-04-28 11:55:04 +0430

    isConfigured.middleware renamed and bug fixed

--- server/middlewares/isConfiguredMiddleware.js
+++
@@ -1,8 +0,0 @@
-const configs = require("../database/configs");
-
-module.exports = async function (req, res, next) {
-  const isConfigured = await configs.isConfigured();
-  if (!isConfigured) return res.redirect("/setup");
-
-  next();
-};
--- server/startup/routes.startup.js
+++ server/startup/routes.startup.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const isConfigured = require("../middlewares/isConfiguredMiddleware");
-const home = require("../routes/home");
+const isConfigured = require("../middlewares/isConfigured.middleware");
+const home = require("../routes/user/home.routes");
 const templatesSrv = require("../services/templates.services");
 
 module.exports = function (app) {
   app.use(express.static(templatesSrv.getPath()));
   app.set("view engine", "ejs");
   app.set("views", templatesSrv.getPath());
-  //app.use(isConfigured);
+  app.use(isConfigured);
   app.use("/", home);
 };
