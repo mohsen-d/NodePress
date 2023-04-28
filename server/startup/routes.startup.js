3cd572055cc9ede5bc6f2f50510b517fd8070d52
Mohsen Dorparasti <dorparasti@gmail.com>
2023-04-28 11:27:53 +0430

    routes.startup updated to add template engine settings

--- server/startup/routes.startup.js
+++ server/startup/routes.startup.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const isConfigured = require("../middlewares/isConfiguredMiddleware");
 const home = require("../routes/home");
+const templatesSrv = require("../services/templates.services");
 
 module.exports = function (app) {
-  app.use(isConfigured);
+  app.use(express.static(templatesSrv.getPath()));
+  app.set("view engine", "ejs");
+  app.set("views", templatesSrv.getPath());
+  //app.use(isConfigured);
   app.use("/", home);
 };
