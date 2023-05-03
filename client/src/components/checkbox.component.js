aee118412fe84f6052d2c4f32aa10411f45d8249
Mohsen Dorparasti <dorparasti@gmail.com>
2023-05-03 22:32:33 +0330

    checkbox.component updated: disabled & value props added

--- client/src/components/checkbox.component.js
+++ client/src/components/checkbox.component.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-export default function Checkbox({ label, name, onChange }) {
+export default function Checkbox({
+  label,
+  name,
+  onChange,
+  disabled,
+  value = false,
+}) {
   return (
     <div>
-      <input onChange={onChange} id={name} type="checkbox" />
+      <input
+        disabled={disabled}
+        onChange={onChange}
+        id={name}
+        type="checkbox"
+        checked={value}
+      />
       <label htmlFor={name}>{label}</label>
     </div>
   );
