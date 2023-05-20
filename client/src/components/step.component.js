445a318f4794c3c1959b124dbc466896e8829d1b
Mohsen Dorparasti <dorparasti@gmail.com>
2023-05-20 18:51:28 +0330

    step(s) component style updated for larger screens

--- client/src/components/step.component.js
+++ client/src/components/step.component.js
@@ -2,21 +2,22 @@ import React from "react";
 import styles from "../assets/step.style.css";
 
 export default function Step({ title, isCompleted, isCurrent = false }) {
-  const stepClassName = styles.step + " col-auto position-relative";
+  const stepClassName = styles.step + " col-auto col-lg-3 position-relative";
   const leftLineClassName =
     styles.left_line +
-    " border-top position-absolute z-0 top-50 end-50 start-0";
+    " border-bottom position-absolute z-0 top-50 end-50 start-0 translate-middle";
 
   const rightLineClassName =
     styles.right_line +
-    " border-top position-absolute z-0 top-50 end-0 start-50";
+    " border-bottom position-absolute z-0 top-50 end-0 start-50 translate-middle";
   const statusClassName =
     styles.status +
     " position-relative z-1 rounded-circle d-inline-block " +
     (isCompleted ? "bg-success" : isCurrent ? "bg-warning" : "bg-dark-subtle");
 
   const titleClassName =
-    "ps-1 pe-1 position-relative bg-white d-inline z-2 " +
+    styles.title +
+    " ps-1 pe-1 position-relative bg-white d-inline z-2 " +
     (isCompleted ? "text-success" : "text-secondary");
 
   return (
--- client/src/components/steps.component.js
+++ client/src/components/steps.component.js
@@ -8,14 +8,20 @@ export default function Steps({ list, current }) {
   const className = styles.steps + " row mb-4 mt-3";
   return (
     <div className={className}>
-      {Object.entries(list).map(([k, s]) => (
-        <Step
-          title={s.title}
-          key={s.title}
-          isCompleted={s.isCompleted}
-          isCurrent={current.title === s.title}
-        />
-      ))}
+      <div className="d-none d-lg-block col-2"></div>
+      <div className="col-12 col-lg-8">
+        <div className="row">
+          {Object.entries(list).map(([k, s]) => (
+            <Step
+              title={s.title}
+              key={s.title}
+              isCompleted={s.isCompleted}
+              isCurrent={current.title === s.title}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="d-none d-lg-block col-2"></div>
     </div>
   );
 }
