42364cf896c9507fc705d5f00350e2fac0dfa31d
Mohsen Dorparasti <dorparasti@gmail.com>
2023-05-21 19:20:32 +0330

    finished.section style updated for larger screens

--- client/src/sections/setup.finished.section.js
+++ client/src/sections/setup.finished.section.js
@@ -4,14 +4,18 @@ export default () => {
   function handleClick() {}
   return (
     <div className="row">
-      <h2 className="mb-2 text-info">SETUP COMPLETED!</h2>
-      <div class="col mb-3">
+      <h2 className="col-12 col-lg-7 offset-lg-2 mb-2 text-info">
+        SETUP COMPLETED!
+      </h2>
+      <div class="col-12 col-lg-7 offset-lg-2 mb-3">
         Hurray! all set. You can now start using your website.
       </div>
-      <div className="d-grid gap-3">
-        <button type="button" class="btn btn-success" onClick={handleClick}>
-          Go to Admin Panel
-        </button>
+      <div class="col-12 col-lg-7 offset-lg-2 mb-3">
+        <div className="d-grid d-lg-block d-lg-flex justify-content-lg-end">
+          <button type="button" class="btn btn-success" onClick={handleClick}>
+            Go to Admin Panel
+          </button>
+        </div>
       </div>
     </div>
   );
